3a15568582f371305a68ad961dfa8072a572e36f
Martín <2002martinvc@gmail.com>
2026-05-06 00:52:29 +0200

    Beer News Generator 7.0

--- .github/scripts/generate_news_script.py
+++ .github/scripts/generate_news_script.py
@@ -38,7 +38,7 @@ payload = {
     }
 }
 
-url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-002:generateContent?key={gemini_key}"
+url = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-001:generateContent?key={gemini_key}"
 req = urllib.request.Request(
     url,
     data=json.dumps(payload).encode(),
